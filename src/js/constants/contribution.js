a58980328d2380b9937e911913a10b7ac6c7f9b5
Huy Duong <huy.duong@smec.com>
2019-03-25 12:04:16 +1100

    / ** * / comments tags

--- src/js/constants/contribution.js
+++ src/js/constants/contribution.js
@@ -1,45 +1,45 @@
 
-/ ** Status: Public * /
+/** Status: Public */
 export const VIEW_STATUS_PUBLIC = 1
-/ ** Status: Private * /
+/** Status: Private */
 export const VIEW_STATUS_PRIVATE = 2
 
-/ ** Status: Public * /
+/** Status: Public */
 export const SOUND_STATUS_PUBLIC = 1
-/ ** Status: Private * /
+/** Status: Private */
 export const SOUND_STATUS_PRIVATE = 2
 
-/ ** Maximum number of tag registrations * /
+/** Maximum number of tag registrations */
 export const TAG_MAX_NUMBER = 10
 
-/ ** Problem type: Spam * /
+/** Problem type: Spam */
 export const PROBLEM_TYPE_SPAM = 1
 
-/ ** Problem type: Improper * /
+/** Problem type: Improper */
 export const PROBLEM_TYPE_INAPPROPRIATE = 2
 
-/ ** Arrival order * /
+/** Arrival order */
 export const ORDER_TYPE_NEW = 1
 
-/ ** Follow number order * /
+/** Follow number order */
 export const ORDER_TYPE_FOLLOW_COUNT = 2
 
-/ ** Status: Public * /
+/** Status: Public */
 export const STATUS_PUBLIC = 1
-/ ** Status: Private * /
+/** Status: Private */
 export const STATUS_PRIVATE = 2
-/ ** Status: Error * /
+/** Status: Error */
 export const STATUS_ERROR = 3
-/ ** Status: Running * /
+/** Status: Running */
 export const STATUS_RUNNING = 4
-/ ** Condition: Remake * /
+/** Condition: Remake */
 export const STATUS_REMEAKE = 5
-/ ** Status: uploading * /
+/** Status: uploading */
 export const STATUS_UPLOADING = 6
-/ ** Status: Created * /
+/** Status: Created */
 export const STATUS_MADE = 7
 
 /** Created */
 export const MAKE_STATUS_MADE = 1
-/ ** not created * /
+/** not created */
 export const MAKE_STATUS_UNCREATED = 2
